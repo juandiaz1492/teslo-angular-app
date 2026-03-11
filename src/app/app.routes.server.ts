559361076134de0b1e709fa9fec2a9881cebd460
Juan Diaz Belda <jdiaz@aunnait.es>
2026-03-11 17:12:40 +0100

    solucion problema server.route prerender

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,34 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
+// export const serverRoutes: ServerRoute[] = [
+//   {
+//     path: '**',
+//     renderMode: RenderMode.Prerender
+//   }
+// ];
+
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'gender/:gender',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return [
+        { gender: 'men' },
+        { gender: 'women' },
+        { gender: 'kids' },
+      ];
+    },
+  },
+  {
+    path: 'admin/products/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Server,
+  },
 ];
